dd5054ed6bc68e151b6f00dddb7cde41db2474b7
Mebin Mathew <mebinmathew006@gmail.com>
2025-09-03 09:31:39 +0530

    Create Qudartic.py

---
+++ Qudartic.py
@@ -0,0 +1,31 @@
+class HashTable:
+    def __init__(self,size=10):
+        self.size = size
+        self.table = [None]*self.size
+        
+    def __hash(self,key):
+        return hash(key)%self.size
+        
+    def insert(self,key,value):
+        index = self.__hash(key)
+        i=0
+        while self.table[index]!=None and self.table[index][0]!=key:
+            index = (index+i*i)%self.size
+            if index == i:
+                raise Exception('Table is full')
+            i+=1
+        self.table[index]=(key,value)
+        
+    def display(self):
+        for i in self.table:
+            if i != None:
+                print(f'{i[0]}:{i[1]}')
+            
+h1 =HashTable()
+h1.insert('dfd',33)
+h1.insert('dgf',33)
+h1.insert('hgh',33)
+h1.insert('jfgh',33)
+h1.insert('ert',33)
+h1.insert('ert',4656)
+h1.display()
